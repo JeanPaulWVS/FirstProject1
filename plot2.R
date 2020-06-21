5f4f5cab1fe932ece8be9eab85afb8e8679a0b0e
JeanPaulWVS <67202310+JeanPaulWVS@users.noreply.github.com>
2020-06-21 18:16:35 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -4,9 +4,9 @@
 
 file_chosse <- "C:\\Users\\jeanpaulwvs\\Desktop\\household_power_consumption.txt"
 data <- read.table(file_chosee, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-data1 <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+data <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
-datetime <- strptime(paste(data1$Date, data1$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+datetime <- strptime(paste(data$Date, data1$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 global <- as.numeric(data1$Global_active_power)
 png("plot2.png", width=480, height=480)
 plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
